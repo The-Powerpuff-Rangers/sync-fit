351185cdfb0c7ba323623bc24123e6c920e71679
Harsh Sharma <77538369+harshsH-28@users.noreply.github.com>
2022-11-05 22:39:28 +0530

    Added new Database

--- server/utils/JobSchedular.js
+++ server/utils/JobSchedular.js
@@ -1,6 +1,7 @@
 const cron = require("node-cron");
 const nodemailer = require("nodemailer");
 const axios = require("axios");
+const User = require("../models/User");
 require("dotenv");
 
 const transporter = nodemailer.createTransport({
@@ -14,25 +15,29 @@ const transporter = nodemailer.createTransport({
 // For Sending Every 24 Hour Request
 
 const every24Hrs = () => {
-  console.log("Task Completed");
-  // After that my task
-  const options = {
-    from: "healthMemoApp32434@outlook.com",
-    to: "harshsharma6419@gmail.com",
-    subject: "Sending you a test email",
-    text: "Welcome to Msit",
-  };
-
-  transporter.sendMail(options, (err, res) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log("Sent: " + res.response);
+  const everyDocument = User.find({}, (err, users) => {
+    users.forEach((user) => {
+      console.log(user.name);
+    });
   });
+  // After that my task
+  // const options = {
+  //   from: "healthMemoApp32434@outlook.com",
+  //   to: "harshsharma6419@gmail.com",
+  //   subject: "Sending you a test email",
+  //   text: "Welcome to Msit",
+  // };
+
+  // transporter.sendMail(options, (err, res) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return;
+  //   }
+  //   console.log("Sent: " + res.response);
+  // });
 };
 
-cron.schedule("50 23 * * *", () => {
+cron.schedule("59 23 * * *", () => {
   every24Hrs();
 });
 
