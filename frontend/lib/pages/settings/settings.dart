324605cd43dce569840786bdae22dcddf5f1374c
Kranium2002 <vidhumathur2002@gmail.com>
2022-11-06 01:38:29 +0530

    Create settings.dart

---
+++ frontend/lib/pages/settings/settings.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          border: Border(top: BorderSide(color: Colors.black, width: 2.0))),
+      child: Row(),
+    );
+  }
+}
+
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(),
+    );
+  }
+}
