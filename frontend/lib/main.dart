9092f61b08fdbe2ed1cf4c5c5a88c4f170940390
2002Bishwajeet <bishwajeet.techmaster@gmail.com>
2022-11-06 01:12:01 +0530

    add bottom navigation bar

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sync_fit/pages/error_screen.dart';
 import 'package:sync_fit/pages/home/home_page.dart';
-import 'package:sync_fit/pages/login/screens/login_screen.dart';
 import 'package:sync_fit/pages/splash/splash_screen.dart';
 import 'package:sync_fit/providers/providers.dart';
 import 'package:sync_fit/utils/routes.dart';
@@ -69,7 +68,7 @@ class AuthChecker extends ConsumerWidget {
         if (data) {
           return const HomePage();
         }
-        return const LoginScreen();
+        return const HomePage();
       },
       error: (error, stackTrace) {
         return ErrorScreen(error: error, stackTrace: stackTrace);
--- frontend/lib/pages/home/home_page.dart
+++ frontend/lib/pages/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sync_fit/utils/app_colors.dart';
 
 final currIndexProvider = StateProvider<int>((ref) {
   return 0;
@@ -11,6 +13,7 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final currIndex = ref.watch(currIndexProvider);
     return Scaffold(
       body: Column(
         children: const [
@@ -19,11 +22,22 @@ class HomePage extends ConsumerWidget {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const [],
-        currentIndex: 0,
+        currentIndex: currIndex,
         onTap: (index) => ref.read(currIndexProvider.notifier).update(
               (state) => index,
             ),
+        enableFeedback: true,
+        type: BottomNavigationBarType.shifting,
+        iconSize: 22,
+        showSelectedLabels: false,
+        fixedColor: AppColors.syncGreen,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.house), label: ''),
+          BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.gear), label: ''),
+          BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.user), label: ''),
+        ],
       ),
     );
   }
