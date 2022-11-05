4b640b4135448235717d8db5148cbf50ef1e555f
2002Bishwajeet <bishwajeet.techmaster@gmail.com>
2022-11-05 22:49:59 +0530

    login ui done

--- frontend/lib/pages/login/screens/login_screen.dart
+++ frontend/lib/pages/login/screens/login_screen.dart
@@ -10,37 +10,47 @@ class LoginScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      body: SafeArea(
-          child: Stack(
-        fit: StackFit.expand,
-        children: [
-          LayoutBuilder(builder: (context, size) {
-            return Column(
+      body: SafeArea(child: LayoutBuilder(builder: (context, size) {
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+                top: 0,
+                left: 0,
+                child: SizedBox(
+                  width: size.maxWidth,
+                  child: Image.asset('assets/images/login_top.png'),
+                )),
+            Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
                   padding: const EdgeInsets.only(left: 16.0, top: 18),
-                  child: AutoSizeText('Welcome to ', style: textTheme.headline3),
+                  child: AutoSizeText(
+                    'Welcome to ',
+                    style: textTheme.headline3!.copyWith(color: Colors.white),
+                  ),
                 ),
                 const SizedBox(
                   height: 16,
                 ),
                 Center(
                   child: SizedBox(
-                    width: size.maxWidth * 0.6,
-                    child: Image.asset(
-                      'assets/images/full_logo.png',
-                    ),
-                  ),
+                      width: size.maxWidth * 0.6,
+                      child: Image.asset(
+                        'assets/images/full_logo.png',
+                        color: Colors.white,
+                      )),
                 ),
                 const Spacer(),
                 Center(
                   child: InkWell(
                     onTap: () {},
                     child: Container(
-                      width: 72,
-                      height: 72,
+                      width: size.maxWidth * 0.6,
+                      height: size.maxHeight / 11,
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
                       decoration: BoxDecoration(
                           border: Border.all(
                             color: Colors.black,
@@ -48,41 +58,42 @@ class LoginScreen extends ConsumerWidget {
                             strokeAlign: StrokeAlign.inside,
                             style: BorderStyle.solid,
                           ),
-                          borderRadius: BorderRadius.circular(5)),
-                      child: Image.asset('assets/icons/fitbit.png'),
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 10.0),
-                    child: AutoSizeText(
-                      'Continue with Fitbit',
-                      style: textTheme.subtitle2!.copyWith(fontFamily: 'SF-Pro Display'),
+                          borderRadius: BorderRadius.circular(36)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image.asset('assets/icons/fitbit.png'),
+                          AutoSizeText(
+                            'Signin with Fitbit',
+                            style: textTheme.subtitle2!.copyWith(
+                                fontFamily: 'SF-Pro Display', fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
                 const Spacer(
-                  flex: 4,
+                  flex: 2,
                 ),
               ],
-            );
-          }),
-          Positioned(
-              bottom: -22,
-              left: -20,
-              child: SizedBox(
-                height: 270,
-                width: 250,
-                child: Lottie.asset(
-                  'assets/lottie/fitness.json',
-                  repeat: true,
-                  // addRepaintBoundary: true,
-                  fit: BoxFit.cover,
-                ),
-              )),
-        ],
-      )),
+            ),
+            Positioned(
+                bottom: -22,
+                left: -20,
+                child: SizedBox(
+                  height: 270,
+                  width: 250,
+                  child: Lottie.asset(
+                    'assets/lottie/fitness.json',
+                    repeat: true,
+                    // addRepaintBoundary: true,
+                    fit: BoxFit.cover,
+                  ),
+                )),
+          ],
+        );
+      })),
     );
   }
 }
