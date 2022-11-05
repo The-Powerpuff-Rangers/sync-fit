2f2bafe589a1cab0816db20294310e8295144b69
Harsh Sharma <77538369+harshsH-28@users.noreply.github.com>
2022-11-06 03:23:46 +0530

    Fixed the error in myApi file

--- server/utils/myApi.js
+++ server/utils/myApi.js
@@ -1,8 +1,6 @@
 const axios = require("axios");
 require("dotenv");
 
-const baseUrl = http://localhost:8080/api/fitness/heartratevar;
-
 const getMyHeartRate = async (userId, date) => {
   try {
     const myHrData = await axios.get(
@@ -24,65 +22,57 @@ const getMyBreathingRate = async (userId, date) => {
   } catch (error) {}
 };
 
-const getMyActivity = async(userId,date)=>{
-    try {
-        const myActivity = await axios.get("http://localhost:8080/api/fitness/activity");
-        return myActivity;
-    } catch (error) {
-        
-    }
-}
+const getMyActivity = async (userId, date) => {
+  try {
+    const myActivity = await axios.get(
+      "http://localhost:8080/api/fitness/activity"
+    );
+    return myActivity;
+  } catch (error) {}
+};
 
-const getMyheartRateVar = async(userId,date)=>{
-    try {
-        const myHrvData = await axios.get("http://localhost:8080/api/fitness/heartratevar");
-        return myHrvData;
-    } catch (error) {
-        
-    }
-}
+const getMyheartRateVar = async (userId, date) => {
+  try {
+    const myHrvData = await axios.get(
+      "http://localhost:8080/api/fitness/heartratevar"
+    );
+    return myHrvData;
+  } catch (error) {}
+};
 
-const getMyWater = async(userId,date)=>{
-    try {
-        const myWaterLog = await axios.get("http://localhost:8080/api/fitness/water");
-        return myWaterLog;
-    } catch (error) {
-        
-    }
-}
+const getMyWater = async (userId, date) => {
+  try {
+    const myWaterLog = await axios.get(
+      "http://localhost:8080/api/fitness/water"
+    );
+    return myWaterLog;
+  } catch (error) {}
+};
 
-const getMySleep = async(userId,date)=>{
-    try {
-        const mySleep = await axios.get("http://localhost:8080/api/fitness/sleep");
-        return mySleep;
-    } catch (error) {
-        
-    }
-}
+const getMySleep = async (userId, date) => {
+  try {
+    const mySleep = await axios.get("http://localhost:8080/api/fitness/sleep");
+    return mySleep;
+  } catch (error) {}
+};
 
-const getMySpo2 = async(userId,date)=>{
-    try {
-        const mySpo2 = await axios.get("http://localhost:8080/api/fitness/spo2");
-        return mySpo2;
-    } catch (error) {
-        
-    }
-}
+const getMySpo2 = async (userId, date) => {
+  try {
+    const mySpo2 = await axios.get("http://localhost:8080/api/fitness/spo2");
+    return mySpo2;
+  } catch (error) {}
+};
 
-const getMyTemp = async(userId,date)=>{
-    try {
-        const myTemp = await axios.get("http://localhost:8080/api/fitness/temp");
-        return myTemp;
-    } catch (error) {
-        
-    }
-}
+const getMyTemp = async (userId, date) => {
+  try {
+    const myTemp = await axios.get("http://localhost:8080/api/fitness/temp");
+    return myTemp;
+  } catch (error) {}
+};
 
-const getMyVo2 = async(userId,date)=>{
-    try {
-        const myVo2 = await axios.get("http://localhost:8080/api/fitness/vo2");
-        return myVo2;
-    } catch (error) {
-        
-    }
-}
+const getMyVo2 = async (userId, date) => {
+  try {
+    const myVo2 = await axios.get("http://localhost:8080/api/fitness/vo2");
+    return myVo2;
+  } catch (error) {}
+};
