5b7ccbdcd31ddbaadc0c38b4870130112a5ce605
Harsh Sharma <77538369+harshsH-28@users.noreply.github.com>
2022-11-06 04:00:11 +0530

    Added Functions in CronTab

--- server/utils/JobSchedular.js
+++ server/utils/JobSchedular.js
@@ -3,6 +3,17 @@ const nodemailer = require("nodemailer");
 const axios = require("axios");
 const User = require("../models/User");
 require("dotenv");
+const {
+  getMyActivity,
+  getMyBreathingRate,
+  getMyHeartRate,
+  getMySleep,
+  getMySpo2,
+  getMyTemp,
+  getMyVo2,
+  getMyWater,
+  getMyheartRateVar,
+} = require("./myApi");
 
 const transporter = nodemailer.createTransport({
   service: "hotmail",
@@ -15,26 +26,38 @@ const transporter = nodemailer.createTransport({
 // For Sending Every 24 Hour Request
 
 const every24Hrs = () => {
+  const date = new Date();
+  const myDate = date.getDate;
   const everyDocument = User.find({}, (err, users) => {
     users.forEach((user) => {
-      console.log(user.name);
+      const docemail = user.docEmail;
+      const user_id = user.userId;
+      const HrData = getMyHeartRate(user_id, date);
+      const HrvData = getMyheartRateVar(user_id, date);
+      const AcData = getMyActivity(user_id, myDate);
+      const BrData = getBreathingRate(user_id, myDate);
+      const SleepData = getMySleep(user_id, myDate);
+      const Spo2Data = getMySpo2(user_id, myDate);
+      const TempData = getMyTemp(user_id, myDate);
+      const Vo2Data = getMyVo2(user_id, myDate);
+      const WaterLog = getMyWater(user_id, myDate);
+
+      const options = {
+        from: "healthMemoApp32434@outlook.com",
+        to: "harshsharma6419@gmail.com",
+        subject: "Sending you a test email",
+        text: "Welcome to Msit",
+      };
+
+      transporter.sendMail(options, (err, res) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Sent: " + res.response);
+      });
     });
   });
-  // After that my task
-  // const options = {
-  //   from: "healthMemoApp32434@outlook.com",
-  //   to: "harshsharma6419@gmail.com",
-  //   subject: "Sending you a test email",
-  //   text: "Welcome to Msit",
-  // };
-
-  // transporter.sendMail(options, (err, res) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return;
-  //   }
-  //   console.log("Sent: " + res.response);
-  // });
 };
 
 cron.schedule("59 23 * * *", () => {
