7c5c5a1d15102f58874d65569d02a3e89a3674b5
Harsh Sharma <77538369+harshsH-28@users.noreply.github.com>
2022-11-06 02:55:58 +0530

    Added BreathingRate myApi

--- server/utils/myApi.js
+++ server/utils/myApi.js
@@ -3,12 +3,23 @@ require("dotenv");
 
 const baseUrl = http://localhost:8080/api/fitness/heartratevar;
 
-const getMyHeartRate = async(userId,date)=>{
-    try {
-       const myHrData = await axios.get("http://localhost:8080/api/fitness/heartratevar");
-       return myHrData;
-    } catch (error) {
-        console.log("Something wrong with myApi");
-        console.log(error);
-    }
+const getMyHeartRate = async (userId, date) => {
+  try {
+    const myHrData = await axios.get(
+      "http://localhost:8080/api/fitness/heartratevar"
+    );
+    return myHrData;
+  } catch (error) {
+    console.log("Something wrong with myApi");
+    console.log(error);
+  }
+};
+
+const getMyBreathingRate = async (userId, date) => {
+  try {
+    const myBreathingRate = await axios.get(
+      "http://localhost:8080/api/fitness/activity"
+    );
+    return myBreathingRate;
+  } catch (error) {}
 };
